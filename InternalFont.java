aaa1c82236e52dfca2b4eaf2229f7a1f4d4add93
dlwub <79503281+dlwub@users.noreply.github.com>
2021-02-23 14:28:16 +0300

    Add files via upload

---
+++ InternalFont.java
@@ -0,0 +1,21 @@
+
+package eternalcalendaritaly;
+import java.awt.Font;
+import java.io.InputStream;
+
+public class InternalFont {
+    public static final String Custom = "Italiana-Regular.ttf";
+
+    public static Font getFont(String fontName, int style, int size) {
+        Font font = null;
+        try {
+            InputStream fontStream = InternalFont.class.getResourceAsStream(fontName);
+            font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            font = font.deriveFont(style, size);
+            fontStream.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return font;
+    }
+}
